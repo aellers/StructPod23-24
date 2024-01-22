e31691164c3af42f663fc73d4cabd0004d030699
aellers <115654510+aellers@users.noreply.github.com>
2024-01-22 15:32:16 +0100

    Update 8.c

--- 8.c
+++ 8.c
@@ -3,50 +3,63 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef _node; 
-typedef struct _node* nodePosition; 
+typedef _node;
+typedef struct _node* nodePosition;
 
-typedef struct _node 
+typedef struct _node
 {
-	int value; 
+	int value;
 	nodePosition leftChild;
-	nodePosition rightChild; 
+	nodePosition rightChild;
 }node;
 
 nodePosition find(int toFind, nodePosition rootPos);
 nodePosition add(int newValue, nodePosition rootPos);
-nodePosition newNode(int newValue); 
+nodePosition newNode(int newValue);
+nodePosition deleteNode(nodePosition root, int value);
 
-void preorder(nodePosition rootPos); 
+void preorder(nodePosition rootPos);
 void inorder(nodePosition rootPos);
-void postorder(nodePosition rootPos); 
+void postorder(nodePosition rootPos);
+
+int levelOrder(node* root);
 
 int main()
 {
-	nodePosition rootPosition = NULL; 
-	//still issues here
+	nodePosition rootPosition = NULL;
+
 	rootPosition = add(5, rootPosition);
 	rootPosition = add(2, rootPosition);
 	rootPosition = add(8, rootPosition);
 
-	preorder(rootPosition); 
+	deleteNode(rootPosition, 5); 
+	preorder(rootPosition);
 
-	return 0; 
+	printf("\n"); 
+	inorder(rootPosition); 
+
+	printf("\n"); 
+	postorder(rootPosition); 
+
+	printf("\n");
+	levelOrder(rootPosition);
+
+	return 0;
 }
 
 nodePosition find(int toFind, nodePosition rootPos)
 {
 	if (rootPos == NULL) {
-		return NULL; 
+		return NULL;
 	}
 	else if (toFind < rootPos->value)
 	{
-		find(toFind, rootPos->leftChild); 
+		find(toFind, rootPos->leftChild);
 	}
 	else if (toFind > rootPos->value)
 	{
-		find(toFind, rootPos->rightChild); 
-	} 
+		find(toFind, rootPos->rightChild);
+	}
 	else
 	{
 		return rootPos;
@@ -59,24 +72,61 @@ nodePosition add(int newValue, nodePosition rootPos)
 		return newNode(newValue); //had before as rootPos = newNode(newValue);, why is that wrong?
 	}
 	else if (newValue <= rootPos->value) {
-		rootPos->leftChild = add(newValue, rootPos->leftChild); 
+		rootPos->leftChild = add(newValue, rootPos->leftChild);
 	}
 	else if (newValue > rootPos->value) {
-		rootPos->rightChild = add(newValue, rootPos->rightChild); 
+		rootPos->rightChild = add(newValue, rootPos->rightChild);
 	}
-	return rootPos; 
+	return rootPos;
 }
 
 nodePosition newNode(int newValue)
 {
-	nodePosition newNode = (nodePosition)malloc(sizeof(node)); 
+	nodePosition newNode = (nodePosition)malloc(sizeof(node));
 	if (newNode == NULL) {
-		printf("Unable to allocate memory\n"); 
-		return NULL; 
+		printf("Unable to allocate memory\n");
+		return NULL;
 	}
 	newNode->value = newValue;
-	newNode->leftChild = NULL; 
-	newNode->rightChild = NULL; 
+	newNode->leftChild = NULL;
+	newNode->rightChild = NULL;
+}
+
+nodePosition deleteNode(nodePosition root, int value)
+{
+	if (root == NULL) {
+		return root; 
+	}
+
+	if (value < root->value) {
+		root->leftChild = deleteNode(root->leftChild, value); 
+	}
+	else if (value > root->value) {
+		root->rightChild = deleteNode(root->rightChild, value); 
+	}
+	else {
+		//0 or 1 child
+		if (root->leftChild == NULL) {
+			nodePosition temp = root->rightChild; 
+			free(root); 
+			return temp; 
+		}
+		else if (root->rightChild == NULL) {
+			nodePosition temp = root->leftChild; 
+			free(root); 
+			return temp; 
+		}
+
+		//2 children
+		nodePosition temp = root->rightChild; 
+		while (temp->leftChild != NULL) {
+			temp = temp->leftChild; 
+		}
+
+		root->value = temp->value; 
+		root->rightChild = deleteNode(root->rightChild, temp->value); 
+	}
+	return root;
 }
 
 void preorder(nodePosition rootPos) //still not working properly 
@@ -92,19 +142,41 @@ void preorder(nodePosition rootPos) //still not working properly
 void inorder(nodePosition rootPos)
 {
 	if (rootPos != NULL) {
-		preorder(rootPos->leftChild);
+		inorder(rootPos->leftChild);
 		printf("%d\n", rootPos->value);
-		preorder(rootPos->rightChild);
+		inorder(rootPos->rightChild);
 	}
 }
 
 void postorder(nodePosition rootPos)
 {
 	if (rootPos != NULL) {
-		preorder(rootPos->leftChild);
-		preorder(rootPos->rightChild);
+		postorder(rootPos->leftChild);
+		postorder(rootPos->rightChild);
 		printf("%d\n", rootPos->value);
 	}
 
 }
 
+int levelOrder(node* root) {
+	if (root == NULL)
+		return;
+
+	node* queue[100] = { 0 };
+	int front = 0, rear = 0;
+
+	queue[rear++] = root;
+
+	while (front < rear) {
+		node* current = queue[front++];
+
+		printf("%d ", current->value);
+
+		if (current->leftChild != NULL)
+			queue[rear++] = current->leftChild;
+
+		if (current->rightChild != NULL)
+			queue[rear++] = current->rightChild;
+	}
+	return 0;
+}
